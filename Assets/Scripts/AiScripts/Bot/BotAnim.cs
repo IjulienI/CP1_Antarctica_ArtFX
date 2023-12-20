91a53b6042b388c7c05125073991fb187f53be37
julien_ssm <89165787+IjulienI@users.noreply.github.com>
2023-12-20 15:46:40 +0100

    [feat] add BotAnim script

---
+++ Assets/Scripts/AiScripts/Bot/BotAnim.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotAnim : MonoBehaviour
+{
+    private AIPathController _controller;
+    private Rigidbody2D _rb;
+    private Animator _animator;
+    private float velocity;
+
+    private void Awake()
+    {
+        _controller = GetComponent<AIPathController>(); 
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        velocity = _rb.velocity.x;
+        if(Mathf.Abs(velocity) > 0.1f )
+        {
+            _animator.SetBool("isWalking", true);
+        }
+        else if(_animator.GetBool("isWalking") != false)
+        {
+            _animator.SetBool("isWalking", false);
+        }
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
+        //Debug.Log(hit.collider.gameObject.layer);
+        if(hit.distance > 1)
+        {
+            _animator.SetBool("isJumping", true);
+        }
+        else if(_animator.GetBool("isJumping") == true)
+        {
+            _animator.SetBool("isJumping", false);
+        }
+    }
+}
