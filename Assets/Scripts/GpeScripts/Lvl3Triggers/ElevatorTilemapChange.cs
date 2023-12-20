24de5c908cd28c6e9962eaaf146955e62306a085
Anthony Boll <anthony.boll@artfx.fr>
2023-12-20 10:29:50 +0100

    [chore] Level 3 MORE DEBRIS

--- Assets/Scripts/GpeScripts/Lvl3Triggers/ElevatorTilemapChange.cs
+++ Assets/Scripts/GpeScripts/Lvl3Triggers/ElevatorTilemapChange.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class ElevatorTilemapChange : MonoBehaviour
 {
     [SerializeField] private GameObject tilemapBefore;
     [SerializeField] private GameObject tilemapAfter;
+    [SerializeField] private ParticleSystem particles;
 
     public void TilemapChange()
     {
+        particles.Play();
         StartCoroutine(GameObject.Find("Virtual Camera").GetComponent<CameraShake>().Shake(0.8f, 16));
+        Invoke("DelayChange", 0.5f);
+    }
 
+    private void DelayChange()
+    {
         tilemapBefore.SetActive(false);
         tilemapAfter.SetActive(true);
         gameObject.GetComponent<SpriteRenderer>().enabled = false;
